db85e4c360723501585d409440be613c35d26d13
apo-ll <marsappollo3@gmaill.com>
2024-03-12 13:18:02 +0300

    he

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        formats: ['image/avif', 'image/webp'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'image.tmdb.org',
                 port: '',
-                pathname: '/**',
+                pathname: '/t/p/**',
             }
         ]
     }
