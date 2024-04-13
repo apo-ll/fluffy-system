2952f9f65b527a25c1b9a0d53f0aefbe6ab9b4c4
Evans Maina <124523386+apo-ll@users.noreply.github.com>
2024-04-13 08:40:24 +0000

    feat: Change single quotes to double quotes in formats and protocol keys and update hostname and pathname to use cloudinary for image hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'image.tmdb.org',
-                port: '',
-                pathname: '/t/p/**',
-            }
-        ]
-    }
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
